acfd47bdc8e4ca552e06c768542e4a618ee4448d
bright <brightlee93@gmail.com>
2020-08-23 12:38:43 +1000

    update quick_picks.py MAX_RANDOM_NUMBER_RANGE and MAX_RANDOM_NUMBER_COUNT

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -1,7 +1,7 @@
 import random
 
 MIN_RANDOM_NUMBER_RANGE = 1
-MAX_RANDOM_NUMBER_RANGE = 3
+MAX_RANDOM_NUMBER_RANGE = 45
 MIN_RANDOM_NUMBER_COUNT = 1
 MAX_RANDOM_NUMBER_COUNT = 6
 
@@ -9,11 +9,12 @@ MAX_RANDOM_NUMBER_COUNT = 6
 number_of_quick_picks = int(input("How many quick picks do you want? "))
 for i in range(1, number_of_quick_picks + 1):
     quick_picks = []
-    for j in range(MIN_RANDOM_NUMBER_COUNT, MAX_RANDOM_NUMBER_RANGE + 1):
-        generated_result = random.randint(MIN_RANDOM_NUMBER_RANGE, MAX_RANDOM_NUMBER_RANGE + 1)
+    for j in range(MIN_RANDOM_NUMBER_COUNT, MAX_RANDOM_NUMBER_COUNT + 1):
+        generated_result = random.randint(MIN_RANDOM_NUMBER_RANGE, MAX_RANDOM_NUMBER_RANGE)
         while generated_result in quick_picks:
-            generated_result = random.randint(MIN_RANDOM_NUMBER_RANGE, MAX_RANDOM_NUMBER_RANGE + 1)
+            generated_result = random.randint(MIN_RANDOM_NUMBER_RANGE, MAX_RANDOM_NUMBER_RANGE)
         quick_picks.append(generated_result)
     quick_picks.sort()
     # print(quick_picks)
     print(" ".join("{:2}".format(generated_result) for generated_result in quick_picks))
+
