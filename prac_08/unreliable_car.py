24f574439e01fb49c57bb5ae8f5717e8fa35a739
bright <brightlee93@gmail.com>
2020-10-13 18:31:37 +1000

    upload car.py from previous prac

---
+++ prac_08/unreliable_car.py
@@ -0,0 +1,19 @@
+from prac_08.car import Car
+import random
+
+
+class UnreliableCar(Car):
+    def __init__(self, name, fuel, reliability):
+        super().__init__(name, fuel)
+        self.reliability = reliability
+
+    def __str__(self):
+        return super().__str__()
+
+    def drive(self, distance):
+        random_number = random.randint(0, 100)
+        if random_number < self.reliability:
+            distance_drive = super().drive(distance)
+        else:
+            distance_drive = 0
+        return distance_drive
