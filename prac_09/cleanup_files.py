8c4e6da1593aab62042a9c41601e9d956456e00d
bright <brightlee93@gmail.com>
2020-10-14 22:52:08 +1000

    add update filename function

--- prac_09/cleanup_files.py
+++ prac_09/cleanup_files.py
@@ -2,27 +2,25 @@
 CP1404/CP5632 Practical
 Demos of various os module examples
 """
-import shutil
 import os
+import re
 
 
 def main():
     """Demo os module functions."""
     print("Starting directory is: {}".format(os.getcwd()))
-
     os.chdir('Lyrics/Christmas')
-
     print("Files in {}:\n{}\n".format(os.getcwd(), os.listdir('.')))
 
-    try:
-        os.mkdir('temp')
-    except FileExistsError:
-        pass
-
     for filename in os.listdir('.'):
-
-        if os.path.isdir(filename):
-            continue
+        for count, character in enumerate(filename):
+            try:
+                if filename[count].islower() and filename[count + 1].isupper():
+                    filename = filename[:count + 1] + "_" + filename[count + 1:]
+                if filename[count].isdigit() and filename[count+1].isalpha():
+                    filename = filename[:count + 1] + "_" + filename[count + 1:]
+            except IndexError:
+                pass
 
         new_name = get_fixed_filename(filename)
         print("Renaming {} to {}".format(filename, new_name))
@@ -30,24 +28,8 @@ def main():
 
 def get_fixed_filename(filename):
     """Return a 'fixed' version of filename."""
-    new_name = filename.replace(" ", "_").replace(".TXT", ".txt")
+    new_name = filename.title().replace(" ", "_").replace(".Txt", ".txt")
     return new_name
 
 
-def demo_walk():
-    """Process all subdirectories using os.walk()."""
-    os.chdir('Lyrics')
-    for directory_name, subdirectories, filenames in os.walk('.'):
-        print("Directory:", directory_name)
-        print("\tcontains subdirectories:", subdirectories)
-        print("\tand files:", filenames)
-        print("(Current working directory is: {})".format(os.getcwd()))
-
-        for filename in filenames:
-            full_name = os.path.join(directory_name, filename)
-            new_name = os.path.join(directory_name, get_fixed_filename(filename))
-            os.rename(full_name, new_name)
-
-
-# main()
-demo_walk()
+main()
